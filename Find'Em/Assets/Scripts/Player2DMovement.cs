4182cbbbf8cf347c099cd44505be698d4b64e0d8
The1BlackSwan <swancd@gmail.com>
2020-10-15 03:30:54 +0200

    3rd

---
+++ Find'Em/Assets/Scripts/Player2DMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2DMovement : MonoBehaviour
+{
+    public GameObject PlayerController;
+    public float movementMultiplier = 2f;
+    private Vector3 origionalPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        origionalPos = gameObject.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        gameObject.transform.position = PlayerController.transform.position * movementMultiplier + origionalPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
