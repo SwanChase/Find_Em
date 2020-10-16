9f96efbade6d15155f24544542d101a8826120e9
KaktusPai <jessebrolsma@gmail.com>
2020-10-17 01:31:28 +0200

    Finished game!

--- Find'Em/Assets/Scripts/FloorIndex.cs
+++ Find'Em/Assets/Scripts/FloorIndex.cs
@@ -4,30 +4,52 @@ using UnityEngine;
 
 public class FloorIndex : MonoBehaviour
 {
-    
+
     public bool running = true;
 
-    public float secondsBetweenNodeLightUp = 10;
+    public float secondsBetweenNodeLightUpGreen = 10;
+    public float secondsBetweenNodeLightUpBlack = 15;
 
     public FloorNode[] floor;
 
-    private int amount;
-    private int maxAmountOfGreen;
+    private int amountOfGreen = 0;
+    private int maxAmountOfGreen = 5;
 
     void Start()
     {
         floor = transform.GetComponentsInChildren<FloorNode>();
-        StartCoroutine(SetRandom());
+        StartCoroutine(SetRandomGreen());
+        StartCoroutine(SetRandomBlack());
     }
 
-    IEnumerator SetRandom()
+    IEnumerator SetRandomGreen()
     {
         while (running)
         {
-            yield return new WaitForSeconds(secondsBetweenNodeLightUp);
-            print("lighting up");
-            floor[Random.Range(0, floor.Length)].GreenPhase();
-            amount ++;
+            if (amountOfGreen <= maxAmountOfGreen)
+            {
+                yield return new WaitForSeconds(secondsBetweenNodeLightUpGreen);
+                floor[Random.Range(0, floor.Length)].GreenPhase();
+                amountOfGreen++;
+                print("amountOfGreen + 1");
+            }
+            else 
+            {
+                amountOfGreen--;
+                print("amountOfGreen - 1");
+            }
+            print(amountOfGreen);
+        }
+    }
+
+    IEnumerator SetRandomBlack()
+    {
+        while (running)
+        {
+            secondsBetweenNodeLightUpBlack = Random.Range(5, 15);
+            yield return new WaitForSeconds(secondsBetweenNodeLightUpBlack);
+            print("Darkening");
+            floor[Random.Range(0, floor.Length)].DangerPhase();
         }
     }
 }
--- Find'Em/Assets/Scripts/Player2D.cs
+++ Find'Em/Assets/Scripts/Player2D.cs
@@ -39,4 +39,36 @@ public class Player2D : MonoBehaviour
         }
 
     }
+
+    void Update()
+    {
+        int layerMask = 1 << 8;
+
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 500, layerMask))
+        {
+            Debug.Log("Detected");
+            GameObject other = hit.collider.gameObject;
+            FloorNode floorNode = other.GetComponent<FloorNode>();
+
+            if (floorNode.currentState == FloorNode.FloorNodeState.Yellow)
+            {
+                Debug.Log("On yellow");
+                GameManagerScript.score += 1;
+            }
+
+            if (floorNode.currentState == FloorNode.FloorNodeState.Black)
+            {
+                Debug.Log("On black");
+                GameManagerScript.score -= 1;
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 500, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
 }
